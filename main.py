343951a86c9ac9f94f43f5774234fbf68106d2a2
moglinen77 <moglinen77@gmail.com>
2024-06-11 23:28:16 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+print(len("Hello Dolli!"))
+print("first 5")
+print("second 7")
+print("summa",5+7)
+print("diff",5-7)
+print("mean",(4+6+9)//3)
+print("понедельник , вторник")
+print((((2*4)+(3*5))**3)//2)
+print((((2*4)+(3*5))**3)/2)
+
+
